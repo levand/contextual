afbb3e3a38271b6aec5c0689839f0d75c578a411
Ben Kamphaus <ben.kamphaus@gmail.com>
2019-06-17 16:26:13 -0600

    minimal deps bumps to get off beta and snapshot

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   know their context"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3196"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.6.3-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]
                    :plugins [[lein-cljsbuild "1.0.5"]
                              [com.cemerick/clojurescript.test "0.3.3"]]
                    :aliases {"test-all" ["do" "test," "cljsbuild" "test"]}
@@ -13,6 +13,6 @@
                                :builds [{:source-paths ["src" "test"]
                                          :compiler {:output-to "target/test-runner.js"
                                                     :optimizations :whitespace
-                                                    :pretty-print true}}]}}}
-  )
+                                                    :pretty-print true}}]}}})
+  
 
